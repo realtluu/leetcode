cc5a7789ace6ad55259e788c3f1200b9d90269ab
thangldw <thangldw@gmail.com>
2023-06-18 14:22:33 +0900

    Create 3.longest-substring-without-repeating-characters.py

---
+++ 3.longest-substring-without-repeating-characters.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode id=3 lang=python3
+#
+# [3] Longest Substring Without Repeating Characters
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = {}
+        l = 0
+        output = 0
+        for r in range(len(s)):
+            if s[r] not in seen:
+                output = max(output,r-l+1)
+            else:
+                if seen[s[r]] < l:
+                    output = max(output,r-l+1)
+                else:
+                    l = seen[s[r]] + 1
+            seen[s[r]] = r
+        return output
+        
+# @lc code=end
+
