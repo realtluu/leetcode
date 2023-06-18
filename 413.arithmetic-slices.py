c7b0b5bb37f12abeaa42c1478734834269ba3c9c
thangldw <thangldw@gmail.com>
2023-06-18 14:22:34 +0900

    Create 413.arithmetic-slices.py

---
+++ 413.arithmetic-slices.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=413 lang=python3
+#
+# [413] Arithmetic Slices
+#
+
+# @lc code=start
+class Solution:
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [0] * n
+        ans = 0
+        for i in range(2, n):
+            if nums[i-1] - nums[i-2] == nums[i] - nums[i-1]:
+                dp[i] = dp[i-1] + 1
+            ans += dp[i]
+        return ans
+        
+# @lc code=end
+
