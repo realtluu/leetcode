3b7eb740897bcf51864102765b8a025b372b6be3
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 45.jump-game-ii.py

---
+++ 45.jump-game-ii.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=45 lang=python3
+#
+# [45] Jump Game II
+#
+
+# @lc code=start
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) <= 1: return 0
+        l, r = 0, nums[0]
+        times = 1
+        while r < len(nums) - 1:
+            times += 1
+            nxt = max(i + nums[i] for i in range(l, r + 1))
+            l, r = r, nxt
+        return times
+        
+# @lc code=end
+
