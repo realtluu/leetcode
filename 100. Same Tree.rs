3a20604807fb86f6c42d8523f110ccbf7bd281f3
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 100. Same Tree.rs

---
+++ 100. Same Tree.rs
@@ -0,0 +1,17 @@
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn is_same_tree(p: Option<Rc<RefCell<TreeNode>>>, q: Option<Rc<RefCell<TreeNode>>>) -> bool {
+        match (p, q) {
+            (None, None) => true,
+            (Some(p), Some(q)) => {
+                let p = p.borrow();
+                let q = q.borrow();
+                p.val == q.val
+                    && Self::is_same_tree(p.left.clone(), q.left.clone())
+                    && Self::is_same_tree(p.right.clone(), q.right.clone())
+            }
+            _ => false,
+        }
+    }
+}
