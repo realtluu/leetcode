3c340537e5bf938f7896955fb259eba5060950e4
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 38.count-and-say.py

---
+++ 38.count-and-say.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=38 lang=python3
+#
+# [38] Count and Say
+#
+
+# @lc code=start
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = '1'
+        for _ in range(n - 1):
+            s = ''.join(str(len(group)) + digit
+                        for group, digit in re.findall(r'((.)\2*)', s))
+        return s    
+        
+# @lc code=end
+
