b7c84661f419f699a0e998927da958df4c0e612e
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 1.two-sum.rs

---
+++ 1.two-sum.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut reverse_nums = HashMap::new();
+
+        for (index, value) in nums.iter().enumerate() {
+            let num = target - *value;
+            if reverse_nums.contains_key(&num) {
+                return vec![reverse_nums[&num], index as i32];
+            }
+            reverse_nums.insert(value, index as i32);
+        }
+        return vec![];
+    }
+}
