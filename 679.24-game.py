18f3776ce208d8a72db46d0a4c77acfce921d10f
thangldw <thangldw@gmail.com>
2023-06-18 14:22:34 +0900

    Create 679.24-game.py

---
+++ 679.24-game.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=679 lang=python3
+#
+# [679] 24 Game
+#
+
+# @lc code=start
+class Solution:
+    def judgePoint24(self, cards: List[int]) -> bool:
+        if len(cards) == 1:
+            return math.isclose(cards[0], 24)
+        return any(self.judgePoint24([x] + rest)
+            for a, b, *rest in itertools.permutations(cards)
+            for x in {a+b, a-b, a*b, b and a/b})
+        
+# @lc code=end
+
