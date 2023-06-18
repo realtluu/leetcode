bf137417e048bb00ca9c45e0b7a0617953afe1cf
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Update 72.edit-distance.py

--- 72.edit-distance.py
+++ 72.edit-distance.py
@@ -6,14 +6,24 @@
 
 # @lc code=start
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        @lru_cache(None)
-        def dp(i, j):
-            if i == 0: return j  # Need to insert j chars
-            if j == 0: return i  # Need to delete i chars
-            if s1[i-1] == s2[j-1]:
-                return dp(i-1, j-1)
-            return min(dp(i-1, j), dp(i, j-1), dp(i-1, j-1)) + 1
+    def minDistance(self, word1, word2):
+        """Dynamic programming solution"""
+        m = len(word1)
+        n = len(word2)
+        table = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m + 1):
+            table[i][0] = i
+        for j in range(n + 1):
+            table[0][j] = j
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    table[i][j] = table[i - 1][j - 1]
+                else:
+                    table[i][j] = 1 + min(table[i - 1][j], table[i][j - 1], table[i - 1][j - 1])
+        return table[-1][-1]
         
 # @lc code=end
 
