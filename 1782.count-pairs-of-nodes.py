039fd99f03dc4716e6770b78782f269199562b90
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 1782.count-pairs-of-nodes.py

---
+++ 1782.count-pairs-of-nodes.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=1782 lang=python3
+#
+# [1782] Count Pairs Of Nodes
+#
+
+# @lc code=start
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]], queries: List[int]) -> List[int]:
+        
+# @lc code=end
+
