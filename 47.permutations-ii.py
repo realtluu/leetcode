b65df744f922357c66a8163c6e940c780e04dd92
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 47.permutations-ii.py

---
+++ 47.permutations-ii.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=47 lang=python3
+#
+# [47] Permutations II
+#
+
+# @lc code=start
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        ans = [[]]
+        for n in nums:
+            new_ans = []
+            for l in ans:
+                for i in range(len(l)+1):
+                    new_ans.append(l[:i]+[n]+l[i:])
+                    if i<len(l) and l[i]==n: break              #handles duplication
+            ans = new_ans
+        return ans
+        
+# @lc code=end
+
