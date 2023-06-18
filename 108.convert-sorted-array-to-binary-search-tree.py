a19498de2926f3c0a19c0a1d1aaf1cc3d9e11648
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 108.convert-sorted-array-to-binary-search-tree.py

---
+++ 108.convert-sorted-array-to-binary-search-tree.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=108 lang=python3
+#
+# [108] Convert Sorted Array to Binary Search Tree
+#
+
+'''
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+'''
+
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        # Time: O(n)
+        # Space: O(n) in the case of skewed binary tree.
+        def convert(left, right):
+            if left > right:
+                return None
+            mid = (left + right) // 2
+            node = TreeNode(nums[mid])
+            node.left = convert(left, mid - 1)
+            node.right = convert(mid + 1, right)
+            return node
+        return convert(0, len(nums) - 1)
+        
+# @lc code=end
+
