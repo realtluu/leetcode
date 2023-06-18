5e7353432ebc439635a320eb42f52c543e47150c
thangldw <thangldw@gmail.com>
2023-06-18 14:22:34 +0900

    Create 236.lowest-common-ancestor-of-a-binary-tree.py

---
+++ 236.lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=236 lang=python3
+#
+# [236] Lowest Common Ancestor of a Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == p or root == q:
+            return root
+        
+        left = right = None
+        if root.left:
+            left = self.lowestCommonAncestor(root.left, p, q)
+        if root.right:
+            right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        else:
+            return left or right
+# @lc code=end
+
