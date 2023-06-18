e1a9e663939513a1f27fde0f043425ba1b0b3575
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 299.bulls-and-cows.py

---
+++ 299.bulls-and-cows.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=299 lang=python3
+#
+# [299] Bulls and Cows
+#
+
+# @lc code=start
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        bulls = 0
+        sh = defaultdict(int)
+        gh = defaultdict(int)
+        for s, g in zip(secret, guess):
+            if s == g:
+                bulls += 1
+            else:
+                sh[s] += 1
+                gh[g] += 1
+        cows = sum(min(sh[k], gh[k]) for k in sh)
+        return "{}A{}B".format(bulls, cows)       
+# @lc code=end
+
