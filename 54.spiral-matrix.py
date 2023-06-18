08b8aa00de651ddedf2ed0474fa3574d46d167b5
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 54.spiral-matrix.py

---
+++ 54.spiral-matrix.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=54 lang=python3
+#
+# [54] Spiral Matrix
+#
+
+# @lc code=start
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+        
+# @lc code=end
+
