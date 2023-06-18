fa1c485e8fc7a159c194b7c2c573c4d8dc767750
thangldw <thangldw@gmail.com>
2023-06-18 14:22:34 +0900

    Create 257.binary-tree-paths.py

---
+++ 257.binary-tree-paths.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=257 lang=python3
+#
+# [257] Binary Tree Paths
+#
+'''
+Given the root of a binary tree, return all root-to-leaf paths in any order.
+A leaf is a node with no children.
+
+'''
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        if not root:
+            return []
+        res, stack = [], [(root, "")]
+        while stack:
+            node, ls = stack.pop()
+            if not node.left and not node.right:
+                res.append(ls+str(node.val))
+            if node.right:
+                stack.append((node.right, ls+str(node.val)+"->"))
+            if node.left:
+                stack.append((node.left, ls+str(node.val)+"->"))
+        return res
+        
+# @lc code=end
+
