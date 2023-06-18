c6a605acedcefb6c1dafef60f1e5b9ecee3ca05c
thangldw <thangldw@gmail.com>
2023-06-18 14:22:35 +0900

    Create 39.combination-sum.py

---
+++ 39.combination-sum.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=39 lang=python3
+#
+# [39] Combination Sum
+#
+
+# @lc code=start
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ret = []
+        self.dfs(candidates, target, [], ret)
+        return ret
+    
+    def dfs(self, nums, target, path, ret):
+        if target < 0:
+            return 
+        if target == 0:
+            ret.append(path)
+            return 
+        for i in range(len(nums)):
+            self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret)
+        
+# @lc code=end
+
